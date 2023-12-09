8394e2977d54d10fe5483cc6467c2dc59ac8c2dd
sauravadiyodi <saurav.adiyodi@gmail.com>
2023-12-09 11:44:30 +0530

    Day 4 part 1

---
+++ day04/d4p1.rs
@@ -0,0 +1,40 @@
+use std::fs;
+
+fn main() {
+    let read = fs::read_to_string("input.txt").unwrap();
+    let data = read.lines();
+    let mut sum: i32 = 0;
+
+    for line in data {
+        let line_split: Vec<&str> = line.split(":").collect();
+        let num_data: Vec<&str> = line_split[1].trim().split(" | ").collect();
+        let card_win: Vec<i32> = num_data[0]
+            .trim()
+            .split_whitespace()
+            .map(|num| num.trim().parse().unwrap())
+            .collect();
+        let card_pos: Vec<i32> = num_data[1]
+            .trim()
+            .split_whitespace()
+            .map(|num| num.trim().parse().unwrap())
+            .collect();
+        let mut count: u32 = 0;
+        for num in &card_pos {
+            if card_win.contains(num) {
+                // println!("Winning num: {num}");
+                count += 1;
+            }
+        }
+
+        // println!(
+        //     "Win: {:?}, Possessed: {:?}, Count: {}",
+        //     card_win, card_pos, count
+        // );
+        if count > 0 {
+            println!("Sum: {sum}, Count: {count}");
+            sum = sum + i32::pow(2, count - 1);
+        }
+    }
+
+    println!("Sum = {}", sum);
+}
